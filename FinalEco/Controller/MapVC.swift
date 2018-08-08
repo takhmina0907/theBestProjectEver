09fc104c9a3218495ef5d81065654735d971af3b
takhmina0907 <lollis0907@gmail.com>
2018-08-08 14:22:57 +0600

    MapView-Not tested

--- FinalEco/Controller/MapVC.swift
+++ FinalEco/Controller/MapVC.swift
@@ -8,15 +8,51 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class MapVC: UIViewController {
     
-     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var locationManager = CLLocationManager()
+    let authorizationStatus = CLLocationManager.authorizationStatus()
+    let regionRadius : Double = 1000
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        mapView.delegate = self
+        locationManager.delegate = self
+        configureLocationServices()
 
+    }
+    
+    @IBAction func MapButtonTapped(_ sender: Any) {
+    }
+    
+}
+extension MapVC:MKMapViewDelegate{
+    func centerMapOnUserLocation(){
+        guard let coordinate = locationManager.location?.coordinate else {
+            return
+        }
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(coordinate, regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+}
+
+extension MapVC:CLLocationManagerDelegate{
+    func configureLocationServices(){
+        if authorizationStatus == .notDetermined{
+            locationManager.requestAlwaysAuthorization()
+        } else{
+            return
+        }
         
     }
-
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        centerMapOnUserLocation()
+    }
+    
 }
+
